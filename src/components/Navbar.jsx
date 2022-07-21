2989ecd732720a68dab80fbce74b7c6979322d8a
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-07-21 11:02:44 +0430

    HamburgerMenu created and also it takes an animation. i use useState Hook to change the state that Menu is open or not

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Navbar.module.css";
+
+// Images
 import logo from "../assets/images/logo-white.svg";
 import down from "../assets/images/down-arrow-svgrepo-com.svg";
+import Hamburger from "../assets/images/hamburger.svg";
+import Cross from "../assets/images/cross.svg";
+import LeftArrow from "../assets/images/arrow.svg";
 
 const Navbar = () => {
+
+    const [ open, setOpen ] = useState(false);
+
+    
+
     return (
         <div>
         <header>
@@ -13,7 +23,7 @@ const Navbar = () => {
 
             <nav>
                 <ul className={styles.HeaderUl}>
-                    <li className={styles.MoreMenu} id="show-production">
+                    <li className={styles.MoreMenu} id="show-product-menu">
                         <a href="/#">محصولات</a>
                         <img src={down} alt="down-arrow" />
                     </li>
@@ -30,7 +40,7 @@ const Navbar = () => {
                         <a href="/#">تماس‌با‌ما</a>
                     </li>
 
-                    <li className={styles.MoreMenu}>
+                    <li className={styles.MoreMenu} id="show-more-menu">
                         <a href="/#">بیش‌تر</a>
                         <img src={down} alt="down-arrow" />
                     </li>
@@ -40,6 +50,55 @@ const Navbar = () => {
                     </li>
                 </ul>
             </nav>
+
+            {/* Hamburger Menu */}
+            <div>
+                
+                <img className={styles.HamburgerIcon}
+                src={Hamburger}
+                alt="hamburger"
+                onClick={() => setOpen(false)}
+                />
+            </div>
+
+           
+            <div className={!open ? styles.HamburgerMenu : styles.CloseHamburgerMenu}>
+
+                <div className={styles.CrossIcon}>
+                    <img src={Cross} alt="cross-icon"
+                    onClick={() => setOpen(true)}
+                    />
+                </div>
+
+
+                <div className={styles.HamburgerItems}>    
+                    <ul>
+                        <li>
+                            <a href="/#">تعرفه‌ها</a>
+                        </li>
+                        <li>
+                            <a href="/#">اپلیکیشن</a>
+                        </li>
+                        <div></div>
+                        <li>
+                            <a href="/#">سوالات متداول</a>
+                        </li>
+                        <li>
+                            <a href="/#">محصولات</a>
+                        </li>
+                        <li>
+                            <a href="/#">تماس با ما</a>
+                        </li>
+                    </ul>
+
+                <div className={styles.MoreItems}>
+                    <a href="/#">زرین‌پال‌من</a>
+                    <img src={LeftArrow} alt="leftarrow" />
+                </div>
+
+                </div>
+            </div>
+
         </header>
         <div style={{width:"100vw", height:"2000px", backgroundColor:"red"}}>
             
