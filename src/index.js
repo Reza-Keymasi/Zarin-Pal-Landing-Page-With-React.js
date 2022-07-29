f0bbe79d8f4b0563655c49c465b59f87ba90d79f
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-07-29 16:37:21 +0430

    BrowserRouter wrapped to <App />

--- src/index.js
+++ src/index.js
@@ -2,19 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom"
 
-// Fonts
-import "./assets/fonts/iranyekanwebmedium.woff";
-import "./assets/fonts/iranyekanwebregular.woff";
-import "./assets/fonts/YekanBakhBold.woff";
-import "./assets/fonts/YekanBakhHeavy.woff";
-import "./assets/fonts/YekanBakhMedium.woff";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 
