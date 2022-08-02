a4a925a410b2b655d890404172da5b5fa425f82f
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-08-02 18:51:37 +0430

    part of Zarin Link component completed

--- src/components/products/ZarinLink.jsx
+++ src/components/products/ZarinLink.jsx
@@ -7,6 +7,13 @@ import Navbar from "../landingPage/Navbar.jsx"
 import Zarinlink from "../../assets/images/zarinlink.svg";
 import DownArrow from "../../assets/images/down-arrow-svgrepo-com.svg";
 
+import User from "../../assets/images/user.svg";
+import ProductBack from "../../assets/images/ProductBack.svg";
+import Like from "../../assets/images/like.svg";
+import Imoji from "../../assets/images/imoji.svg";
+import Coupon from "../../assets/images/coupon.svg";
+import Order from "../../assets/images/order.svg";
+
 const ZarinLink = () => {
     return (
         <div>
@@ -44,14 +51,122 @@ const ZarinLink = () => {
             </main>
 
             <article>
-                <div>
+                <div className={styles.Title}>
                     <h2>
                         ویژگی‌های‌زرین‌لینک
                     </h2>
                 </div>
 
-                <div>
-                    
+                <div className={styles.Options}>
+                    <div className={styles.Option}  id={styles.OptionOne}>
+                        <div className={styles.Container}>
+                            <div>
+                                <img src={User} alt="User" />
+                                <h4>لینک‌پرداخت‌شخصی</h4>
+                            </div>
+                            <p>
+                                برای خرید، پرداخت، یا انتقال مبلغ به حساب تعریف شده،
+                                می‌‌توان از لینک پرداخت شخصی و بدون مبلغ از پیش تعیین شده استفاده کرد.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.Option} id={styles.OptionTwo}>
+                        <div className={styles.Container}>
+                            <div>
+                                <img src={ProductBack} alt="product-back" />
+                                <h4>
+                                مدیریت‌محصولات
+                                </h4>
+                            </div>
+                            <p>
+                                با زرین‌‌لینک می‌‌توان به صورت جداگانه، برای هر محصول
+                                فروشگاه سقف ثبت سفارش تعیین کرد و یک
+                                لینک پرداخت با قیمت مشخص و مجزا به آن اختصاص داد.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.Option} id={styles.OptionThree}>
+                        <div className={styles.Container}>
+                            <div>
+                                <img src={Like} alt="person-info" />
+                                <h4>دریافت‌اطلاعات‌مشتریان</h4>
+                            </div>
+                            <p>
+                                فروشندگان می‌‌توانند با مشاهده و دریافت اطلاعات پرداخت‌‌کنندگان خود،
+                                مانند نام، نام خانوادگی، آدرس ایمیل و شماره تماس،
+                                با مشتریان خود در ارتباط باشند.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.Option} id={styles.OptionFour}>
+                        <div className={styles.Container}>
+                            <div>
+                                <img src={Imoji} alt="payment-complitig" />
+                                <h4>تکمیل‌فرآیند‌خرید</h4>
+                            </div>
+                            <p>
+                                پس از این‌‌که مشتری پرداخت خود را نهایی می‌‌کند، فروشنده
+                                می‌‌تواند پیامی با مضمون دلخواه و مرتبط با 
+                                تراکنش‌‌های موفق یا ناموفق برای مشتری خود ارسال کند.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.Option} id={styles.OptionFive}>
+                        <div className={styles.Container}>
+                            <div>
+                                <img src={Coupon} alt="coupon" />
+                                <h4>کدتخفیف</h4>
+                            </div>
+                            <p>
+                                فروشنده به‌‌راحتی می‌‌تواند لینک کد تخفیف موردنظر خود را
+                                در زرین‌‌لینک تعریف و مشخص کند و در اختیار مشتریان خود قرار دهد.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.Option} id={styles.OptionSix}>
+                        <div className={styles.Container}>
+                            <div>
+                                <img src={Order} alt="" />
+                                <h4>تعریف‌تعداد‌سفارش</h4>
+                            </div>
+                            <p>
+                                در زرین‌‌لینک محدودیتی در تعریف تعداد سفارش برای هر محصول وجود ندارد.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.Option} id={styles.OptionSeven}>
+                        <div className={styles.Container}>
+                            <div>
+                                <img src={Imoji} alt="" />
+                                <h4>عدم‌محدودیت‌انتقال‌وجه</h4>
+                            </div>
+                            <p>
+                                با سقف انتقال وجه ۵۰ میلیون تومانی با زرین‌‌لینک، دیگر
+                                نیازی نیست مشتریان برای انتقال مبالغ بالا به
+                                خودپرداز یا شعب بانکی مراجعه کنند.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.Option} id={styles.OptionEight}>
+                        <div className={styles.Container}>
+                            <div>
+                                <img src={Imoji} alt="" />
+                                <h4>مدیریت‌امورمالی‌و‌تراکنش‌‌ها</h4>
+                            </div>
+                            <p>
+                                در پنل مدیریتی زرین‌‌پال، گزارش دقیقی از تمام تراکنش‌‌ها
+                                بر اساس محصول، تاریخ، مبلغ و نام پرداخت‌‌کننده قابل مشاهده است
+                                و تایید تراکنش‌‌‌‌ها، نیازی به چک کردن فیش‌‌های واریزی با بانک مبدا نخواهد داشت.
+                            </p>
+                        </div>
+                    </div>
                 </div>
 
             </article>
